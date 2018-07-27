ecb6f278eeb6b39ba3288860847bf7d6797d3a3e
vmadenur <vmadenur@local>
2018-07-27 17:59:47 +0000

    converted model to conv

--- keras-fashion/perceptron-linear.py
+++ keras-fashion/perceptron-linear.py
@@ -1,7 +1,7 @@
 import numpy
 from keras.datasets import fashion_mnist
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Dense, Flatten, Dropout, BatchNormalization, Activation, Conv2D, MaxPooling2D
 from keras.utils import np_utils
 import wandb
 from wandb.keras import WandbCallback
@@ -10,6 +10,8 @@ from wandb.keras import WandbCallback
 run = wandb.init()
 config = run.config
 config.epochs = 10
+config.loss = "categorical_crossentropy"
+config.optimizer = "adam"
 
 # load data
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
@@ -19,6 +21,15 @@ img_height = X_train.shape[2]
 labels =["T-shirt/top","Trouser","Pullover","Dress",
     "Coat","Sandal","Shirt","Sneaker","Bag","Ankle boot"]
 
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train/=255
+X_test/=255
+
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
@@ -26,12 +37,54 @@ y_test = np_utils.to_categorical(y_test)
 num_classes = y_train.shape[1]
 
 # create model
-model=Sequential()
-model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(num_classes))
-model.compile(loss='mse', optimizer='adam',
-                metrics=['accuracy'])
+#model=Sequential()
+#model.add(Flatten(input_shape=(img_width,img_height)))
+#model.add(Dense(num_classes))
+#model.compile(loss='mse', optimizer='adam',
+#                metrics=['accuracy'])
 
+model=Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(img_width, img_height, 1)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2), padding="same"))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(64,(3, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2), padding="same"))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(128,(2, 2)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (2, 2)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2), padding="same"))
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes))
+model.add(BatchNormalization())
+model.add(Activation('softmax'))
+
+model.compile(loss=config.loss, optimizer=config.optimizer,
+               metrics=['accuracy'])
+
+#model.summary()
 # Fit the model
 model.fit(X_train, y_train, epochs=config.epochs, validation_data=(X_test, y_test),
                     callbacks=[WandbCallback(data_type="image", labels=labels)])
